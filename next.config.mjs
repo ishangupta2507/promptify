5ca6fa593452b4964e661749102c018547b68e69
ishangupta2507 <164048495+ishangupta2507@users.noreply.github.com>
2024-08-19 12:44:49 +0530

    updated mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
-    serverComponentsExternalPackages: ["mongoose"],
+    serverComponentsExternalPackages: ["mongoose"], // Keep this if needed
   },
   images: {
     domains: ['lh3.googleusercontent.com'],
